760abdfe9e5a45b9b65626c5e03dfc3dc0f074d3
Tian Luo <tianluo@live.com>
2018-01-30 04:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function whisper(string)
 {
   return string.toLowerCase()
 }
+
+function logShout(string)
+{
+  console.log(string.toUpperCase)
+}
+
+function logwhisper()
+{
+  console.log(string.toLowerCase)
+}
