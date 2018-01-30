688559fd965dbd889efc4b6910e8d7f274e8c59c
Tian Luo <tianluo@live.com>
2018-01-30 05:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma (string)
 {
   
   if string === lowercase
-    return "I can\'t hear you!"
+     "I can\'t hear you!"
   else
     if string === uppercase
       return "YES INDEED!"
