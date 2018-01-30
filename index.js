e981a73f3b7c93dea69b335db5c7b054aeb22a43
Tian Luo <tianluo@live.com>
2018-01-30 04:54:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function whisper(string)
 
 function logShout(string)
 {
-  console.log(string.toUpperCase)
+  console.log('string.toUpperCase')
 }
 
 function logwhisper(string)
 {
-  console.log(string.toLowerCase)
+  console.log('string.toLowerCase')
 }
